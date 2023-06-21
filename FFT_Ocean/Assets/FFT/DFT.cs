dcb710efacfb9d6ee2b3edb32dcebbac59ac31cb
AShine17cm <304728651@qq.com>
2023-06-21 22:42:03 +0800

    傅里叶变换  理论和 CPU 算法演示

---
+++ FFT_Ocean/Assets/FFT/DFT.cs
@@ -0,0 +1,239 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Math = System.Math;
+public class DFT_Helper
+{
+    public struct COMPLEX
+    {
+        public float real;
+        public float imag;
+    }
+    /*-------------------------------------------------------------------------
+   Perform a 2D FFT inplace given a complex 2D array
+   The direction dir, 1 for forward, -1 for reverse
+   The size of the array (nx,ny)
+   Return false if there are memory problems or
+      the dimensions are not powers of 2
+*/
+    public static int FFT2D(COMPLEX[][] c, int nx, int ny, int dir,bool isFFT)
+    {
+        
+        int i, j;
+        int m=0, twopm=0;
+        float[] real = new float[nx];
+        float[] imag = new float[nx];
+
+        /* Transform the rows */
+        if (Powerof2(nx, ref m, ref twopm)==0 || twopm != nx)
+            return 0;
+        for (j = 0; j < ny; j++)//逐行
+        {
+            for (i = 0; i < nx; i++)        //提取到临时的 行数组
+            {
+                real[i] = c[i][j].real;
+                imag[i] = c[i][j].imag;
+            }
+            if(isFFT)
+            {
+                FFT(dir, m, real, imag);        //变换 一行
+            }
+            else
+            {
+                DFT(dir, twopm, real, imag);
+            }
+            for (i = 0; i < nx; i++)        //写出结果 到原位置
+            {
+                c[i][j].real = real[i];
+                c[i][j].imag = imag[i];
+            }
+        }
+        //return 1;
+        /* Transform the columns */
+        real = new float[ny];
+        imag = new float[ny];
+        if (Powerof2(ny, ref m, ref twopm)==0 || twopm != ny)
+            return 0;
+        for (i = 0; i < nx; i++)//逐列
+        {
+            for (j = 0; j < ny; j++)
+            {
+                real[j] = c[i][j].real;
+                imag[j] = c[i][j].imag;
+            }
+            if(isFFT)
+            {
+                FFT(dir, m, real, imag); //写出结果到列
+            }
+            else
+            {
+                DFT(dir, twopm, real, imag);
+            }
+            for (j = 0; j < ny; j++)
+            {
+                c[i][j].real = real[j];
+                c[i][j].imag = imag[j];
+            }
+        }
+
+        return 1;
+    }
+    //离散傅里叶变换
+    // x1实部， y1虚部
+
+    public static bool DFT(int dir, int m, float[] x1, float[] y1)
+    {
+        int i, k;
+        float arg;
+        float cosarg, sinarg;
+        float[] x2 = new float[m];
+        float[] y2 = new float[m];
+
+
+        for (i = 0; i < m; i++)
+        {
+            x2[i] = 0;
+            y2[i] = 0;
+            arg = -dir * 2.0f * 3.141592654f * i / m;
+            for (k = 0; k < m; k++)
+            {
+                cosarg = Mathf.Cos(k * arg);
+                sinarg = Mathf.Sin(k * arg);
+                x2[i] += (x1[k] * cosarg - y1[k] * sinarg);
+                y2[i] += (x1[k] * sinarg + y1[k] * cosarg);
+            }
+        }
+
+        /* Copy the data back */
+        if (dir == 1)
+        {
+            for (i = 0; i < m; i++)
+            {
+                x1[i] = x2[i] / m;
+                y1[i] = y2[i] / m;
+            }
+        }
+        else
+        {
+            for (i = 0; i < m; i++)
+            {
+                x1[i] = x2[i];
+                y1[i] = y2[i];
+            }
+        }
+
+        return true;
+    }
+
+    /*
+   This computes an in-place complex-to-complex FFT 
+   x and y are the real and imaginary arrays of 2^m points.
+   dir =  1 gives forward transform
+   dir = -1 gives reverse transform 
+*/
+    public static bool FFT(int dir, int m, float[] x, float[] y)
+    {
+        int n, i, i1, j, k, i2, l, l1, l2;
+        float c1, c2, tx, ty, t1, t2, u1, u2, z;
+
+        /* Calculate the number of points */
+        n = 1;
+        for (i = 0; i < m; i++)
+            n *= 2;
+
+        /* Do the bit reversal */
+        i2 = n >> 1;
+        j = 0;
+        for (i = 0; i < n - 1; i++)
+        {
+            if (i < j)
+            {
+                tx = x[i];
+                ty = y[i];
+                x[i] = x[j];
+                y[i] = y[j];
+                x[j] = tx;
+                y[j] = ty;
+            }
+            k = i2;
+            while (k <= j)
+            {
+                j -= k;
+                k >>= 1;
+            }
+            j += k;
+        }
+
+        /* Compute the FFT */
+        c1 = -1.0f;
+        c2 = 0.0f;
+        l2 = 1;
+        for (l = 0; l < m; l++)
+        {
+            l1 = l2;
+            l2 <<= 1;
+            u1 = 1.0f;
+            u2 = 0.0f;
+            for (j = 0; j < l1; j++)
+            {
+                for (i = j; i < n; i += l2)
+                {
+                    i1 = i + l1;
+                    t1 = u1 * x[i1] - u2 * y[i1];
+                    t2 = u1 * y[i1] + u2 * x[i1];
+                    x[i1] = x[i] - t1;
+                    y[i1] = y[i] - t2;
+                    x[i] += t1;
+                    y[i] += t2;
+                }
+                z = u1 * c1 - u2 * c2;
+                u2 = u1 * c2 + u2 * c1;
+                u1 = z;
+            }
+            c2 = Mathf.Sqrt((1.0f - c1) / 2.0f);
+            if (dir == 1)
+                c2 = -c2;
+            c1 = Mathf.Sqrt((1.0f + c1) / 2.0f);
+        }
+
+        /* Scaling for forward transform */
+        if (dir == 1)
+        {
+            for (i = 0; i < n; i++)
+            {
+                x[i] /= n;
+                y[i] /= n;
+            }
+        }
+
+        return true;
+    }
+
+    /*-------------------------------------------------------------------------
+   Calculate the closest but lower power of two of a number
+   twopm = 2**m <= n
+   Return TRUE if 2**m == n
+    */
+    static int Powerof2(int n, ref int m, ref int twopm)
+    {
+        if (n <= 1)
+        {
+            m = 0;
+            twopm = 1;
+            return 0;
+        }
+
+        m = 1;
+        twopm = 2;
+        do
+        {
+            m++;
+            twopm *= 2;
+        } while (2 * twopm <= n);
+
+        if (twopm != n)
+            return 0;
+        else
+            return 1;
+    }
+}
---
+++ FFT_Ocean/Assets/FFT/Test_FT.cs
@@ -0,0 +1,261 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Complex = DFT_Helper.COMPLEX;
+/*
+ 用 cpu 实现，测试 傅里叶变换
+ */
+public class Test_FT : MonoBehaviour
+{
+    public Texture2D source;
+    Texture2D fourierTex;
+    Texture2D resultTex;
+
+    public bool doTest = false;
+    public bool useFFT = true;
+
+    public RawImage srcImg;
+    public RawImage fourierImg;
+    public RawImage resultImg;
+    public RectTransform trOverlay;
+    //public bool applyOverlay = true;
+    public float brightCorrect = 5f;
+    [Range(0, 0.9f)]
+    public float filter_High = 0;    
+    [Range(0.6f, 1.001f)]
+    public float filter_Low = 1.001f;
+    int dir = 1;                    //傅里叶变换的方向
+    Complex[][] fourierRaw_R;         //原始的 频域数据
+    Complex[][] fourierRaw_G;
+    Complex[][] fourierRaw_B;
+    Complex[][] fourierFiltered_R;    //滤波之后的数据
+    Complex[][] fourierFiltered_G;
+    Complex[][] fourierFiltered_B;
+    float last_filter_High;
+    float last_filter_Low;
+    void Start()
+    {
+        srcImg.texture = source;
+        last_filter_High = filter_High;
+        last_filter_Low = filter_Low;
+        int w = source.width;
+        int h = source.height;
+        fourierTex = new Texture2D(w, h, TextureFormat.RGBA32, false);
+        fourierTex.filterMode = FilterMode.Point;
+        resultTex = new Texture2D(w, h, TextureFormat.RGBA32, false);
+        resultTex.filterMode = FilterMode.Bilinear;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            Vector3 pos = Input.mousePosition;
+            Vector2 localPos;
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(srcImg.GetComponent<RectTransform>(), pos, null, out localPos);
+            trOverlay.anchoredPosition = localPos;
+            doTest = true;
+        }
+        if (fourierRaw_R != null)
+        {
+            if (Mathf.Abs(last_filter_High - filter_High) > float.Epsilon)
+            {
+                last_filter_High = filter_High;
+                Display(source.width, Vector2Int.zero);
+                fourierTex.Apply();
+                resultTex.Apply();
+            }
+            if (Mathf.Abs(last_filter_Low - filter_Low) > float.Epsilon)
+            {
+                last_filter_Low = filter_Low;
+                Display(source.width, Vector2Int.zero);
+                fourierTex.Apply();
+                resultTex.Apply();
+            }
+        }
+        if (doTest)
+        {
+            doTest = false;
+
+            int size = source.width;
+            Vector2Int offset = Vector2Int.zero;
+            fourierFiltered_R = new Complex[size][];
+            fourierFiltered_G = new Complex[size][];
+            fourierFiltered_B = new Complex[size][];
+            fourierRaw_R = new Complex[size][];
+            fourierRaw_G = new Complex[size][];
+            fourierRaw_B = new Complex[size][];
+            DoQuad(size, offset);
+
+            fourierTex.Apply();
+            resultTex.Apply();
+            fourierImg.texture = fourierTex;
+            resultImg.texture = resultTex;
+        }
+    }
+    void DoQuad(int ofSize, Vector2Int offset)
+    {
+        fourierRaw_R = new Complex[ofSize][];
+        for (int r = 0; r < ofSize; r++)
+        {
+            fourierRaw_R[r] = new Complex[ofSize];
+            fourierRaw_G[r] = new Complex[ofSize];
+            fourierRaw_B[r] = new Complex[ofSize];
+            fourierFiltered_R[r] = new Complex[ofSize];
+            fourierFiltered_G[r] = new Complex[ofSize];
+            fourierFiltered_B[r] = new Complex[ofSize];
+            for (int c = 0; c < ofSize; c++)
+            {
+                Vector2Int uv = offset + new Vector2Int(r, c);
+                Color color = source.GetPixel(uv.x, uv.y);
+                Complex complex = new Complex();
+                complex.real = color.r;
+                fourierRaw_R[r][c] = complex;  //写到对应位置
+                complex.real = color.g;
+                fourierRaw_G[r][c] = complex;  //写到对应位置
+                complex.real = color.b;
+                fourierRaw_B[r][c] = complex;  //写到对应位置
+            }
+        }
+        //叠加 overlay 信号
+        //if (applyOverlay)
+        //{
+        //    Vector2 size = trOverlay.sizeDelta;
+        //    Vector2 pos = trOverlay.anchoredPosition;
+        //    Vector2 srcSize = srcImg.GetComponent<RectTransform>().sizeDelta;
+        //    float atx0 = (srcSize.x / 2 + pos.x - size.x / 2) / srcSize.x;
+        //    float atx1 = (srcSize.x / 2 + pos.x + size.x / 2) / srcSize.x;
+        //    float aty0 = (srcSize.y / 2 + pos.y - size.y / 2) / srcSize.y;
+        //    float aty1 = (srcSize.y / 2 + pos.y + size.y / 2) / srcSize.y;
+        //    int x0 = Mathf.Clamp((int)(atx0 * ofSize), 0, ofSize);
+        //    int x1 = Mathf.Clamp((int)(atx1 * ofSize), 0, ofSize);
+        //    int y0 = Mathf.Clamp((int)(aty0 * ofSize), 0, ofSize);
+        //    int y1 = Mathf.Clamp((int)(aty1 * ofSize), 0, ofSize);
+        //    Color overlayColor = trOverlay.GetComponent<Image>().color;
+        //    for (int r = x0; r < x1; r++)
+        //    {
+        //        for (int c = y0; c < y1; c++)
+        //        {
+        //            Complex cp = fourierRaw_R[r][c];
+        //            Vector2Int uv = offset + new Vector2Int(r, c);
+        //            cp.real += overlayColor.r;
+        //            cp.real = Mathf.Clamp(cp.real, 0, 1);
+        //            fourierRaw_R[r][c] = cp;  //写到对应位置
+        //        }
+        //    }
+        //}
+        DFT_Helper.FFT2D(fourierRaw_R, ofSize, ofSize, dir, useFFT);      //转换到 频率
+        DFT_Helper.FFT2D(fourierRaw_G, ofSize, ofSize, dir, useFFT);      //转换到 频率
+        DFT_Helper.FFT2D(fourierRaw_B, ofSize, ofSize, dir, useFFT);      //转换到 频率
+        Display(ofSize, offset);
+    }
+    void Display(int ofSize, Vector2Int offset)
+    {
+        //拷贝一份 频率数据
+        for (int k = 0; k < ofSize; k++)
+        {
+            for (int L = 0; L < ofSize; L++)
+            {
+                fourierFiltered_R[k][L] = fourierRaw_R[k][L];
+                fourierFiltered_G[k][L] = fourierRaw_G[k][L];
+                fourierFiltered_B[k][L] = fourierRaw_B[k][L];
+            }
+        }
+        //滤波
+        Vector2Int center = new Vector2Int(1, 1) * ofSize / 2;
+        float high = ofSize / 2;
+        high =filter_High* high * high;
+        for (int m = 0; m < ofSize; m++)
+        {
+            for (int n = 0; n < ofSize; n++)
+            {
+                if ((new Vector2Int(m, n) - center).sqrMagnitude < high)
+                {
+                    fourierFiltered_R[m][n] = new Complex();
+                    fourierFiltered_G[m][n] = new Complex();
+                    fourierFiltered_B[m][n] = new Complex();
+                }
+            }
+        }
+        float low = ofSize / 2;
+        low = filter_Low * low * low * 2;//对角线
+        for (int m = 0; m < ofSize; m++)
+        {
+            for (int n = 0; n < ofSize; n++)
+            {
+                if ((new Vector2Int(m, n) - center).sqrMagnitude > low)
+                {
+                    fourierFiltered_R[m][n] = new Complex();
+                    fourierFiltered_G[m][n] = new Complex();
+                    fourierFiltered_B[m][n] = new Complex();
+                }
+            }
+        }
+
+        //滤波之后的 频域数据
+        for (int r = 0; r < ofSize; r++)
+        {
+            Complex[] row_R = fourierFiltered_R[r];
+            Complex[] row_G = fourierFiltered_G[r];
+            Complex[] row_B = fourierFiltered_B[r];
+            for (int c = 0; c < ofSize; c++)
+            {
+                Vector2Int uv = offset + new Vector2Int(r, c);
+                float real_R = row_R[c].real;
+                float imag_R = row_R[c].imag;
+                float val_R;
+                val_R = Mathf.Sqrt(real_R * real_R + imag_R * imag_R);
+                val_R = Mathf.Pow(val_R, 1 / brightCorrect);
+
+                float real_G = row_G[c].real;
+                float imag_G = row_G[c].imag;
+                float val_G;
+                val_G = Mathf.Sqrt(real_G * real_G + imag_G * imag_G);
+                val_G = Mathf.Pow(val_G, 1 / brightCorrect);
+
+                float real_B = row_B[c].real;
+                float imag_B = row_B[c].imag;
+                float val_B;
+                val_B = Mathf.Sqrt(real_B * real_B + imag_B * imag_B);
+                val_B = Mathf.Pow(val_B, 1 / brightCorrect);
+
+
+                Color color = new Color(val_R, val_G, val_B, 1);
+                fourierTex.SetPixel(uv.x, uv.y, color);
+            }
+        }
+
+        DFT_Helper.FFT2D(fourierFiltered_R, ofSize, ofSize, -dir, useFFT); //将信号 从频域变换回来
+        DFT_Helper.FFT2D(fourierFiltered_G, ofSize, ofSize, -dir, useFFT); //将信号 从频域变换回来
+        DFT_Helper.FFT2D(fourierFiltered_B, ofSize, ofSize, -dir, useFFT); //将信号 从频域变换回来
+        //变回 颜色空间的数据
+        for (int r = 0; r < ofSize; r++)
+        {
+            Complex[] row_R = fourierFiltered_R[r];
+            Complex[] row_G = fourierFiltered_G[r];
+            Complex[] row_B = fourierFiltered_B[r];
+            for (int c = 0; c < ofSize; c++)
+            {
+                Vector2Int uv = offset + new Vector2Int(r, c);
+                float real_R = row_R[c].real;
+                float imag_R = row_R[c].imag;
+                float val_R;
+                val_R = Mathf.Sqrt(real_R * real_R + imag_R * imag_R);
+
+                float real_G = row_G[c].real;
+                float imag_G = row_G[c].imag;
+                float val_G;
+                val_G = Mathf.Sqrt(real_G * real_G + imag_G * imag_G);
+
+                float real_B = row_B[c].real;
+                float imag_B = row_B[c].imag;
+                float val_B;
+                val_B = Mathf.Sqrt(real_B * real_B + imag_B * imag_B);
+
+                Color color = new Color(val_R, val_G, val_B, 1);
+                resultTex.SetPixel(uv.x, uv.y, color);
+            }
+        }
+    }
+}
